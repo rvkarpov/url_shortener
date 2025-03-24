a187a6a7453a6e2f24e870b11ea154f49bf1aa1f
rkarpov <rkarpov@ptsecurity.com>
2025-03-24 14:00:16 +0300

    iter8: add compress middleware

---
+++ internal/middleware/compress.go
@@ -0,0 +1,94 @@
+package middleware
+
+import (
+	"compress/gzip"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type gzipWriter struct {
+	http.ResponseWriter
+	writer *gzip.Writer
+}
+
+func (w *gzipWriter) Write(b []byte) (int, error) {
+	return w.writer.Write(b)
+}
+
+func (w *gzipWriter) Close() {
+	w.writer.Close()
+}
+
+func newGzipWriter(rsp http.ResponseWriter) (*gzipWriter, error) {
+	return &gzipWriter{ResponseWriter: rsp, writer: gzip.NewWriter(rsp)}, nil
+}
+
+type gzipReader struct {
+	closer io.ReadCloser
+	reader *gzip.Reader
+}
+
+func (r *gzipReader) Read(p []byte) (n int, err error) {
+	return r.reader.Read(p)
+}
+
+func (r *gzipReader) Close() error {
+	if err := r.reader.Close(); err != nil {
+		return err
+	}
+	return r.closer.Close()
+}
+
+func newGzipReader(rqs *http.Request) (*gzipReader, error) {
+	gzr, err := gzip.NewReader(rqs.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &gzipReader{rqs.Body, gzr}, nil
+}
+
+func Compress(h http.HandlerFunc) http.HandlerFunc {
+	return func(rsp http.ResponseWriter, rqs *http.Request) {
+		//
+		// uncompress request
+		//
+		contentEncoding := rqs.Header.Get("Content-Encoding")
+		needUncompress := strings.Contains(contentEncoding, "gzip")
+
+		if needUncompress {
+			gzr, err := newGzipReader(rqs)
+			if err != nil {
+				http.Error(rsp, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer gzr.Close()
+
+			rqs.Header.Del("Content-Encoding")
+			rqs.Body = gzr
+		}
+
+		//
+		// compress responce
+		//
+		encoding := rqs.Header.Get("Accept-Encoding")
+		contentType := rqs.Header.Get("Content-Type")
+		allowCompressRsp := strings.Contains(encoding, "gzip") && (contentType == "application/json" || contentType == "text/html")
+
+		if !allowCompressRsp {
+			h.ServeHTTP(rsp, rqs)
+			return
+		}
+
+		gzw, err := newGzipWriter(rsp)
+		if err != nil {
+			http.Error(rsp, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer gzw.Close()
+
+		rsp.Header().Set("Content-Encoding", "gzip")
+		h.ServeHTTP(gzw, rqs)
+	}
+}
---
+++ internal/middleware/compress_test.go
@@ -0,0 +1,113 @@
+package middleware
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi"
+	"github.com/rvkarpov/url_shortener/internal/handler"
+	"github.com/rvkarpov/url_shortener/internal/mocks"
+	"github.com/rvkarpov/url_shortener/internal/service"
+	"github.com/rvkarpov/url_shortener/internal/testutils"
+	"github.com/stretchr/testify/assert"
+)
+
+func compress(in string) string {
+	buf := bytes.NewBuffer(nil)
+	gzw := gzip.NewWriter(buf)
+	gzw.Write([]byte(in))
+	gzw.Close()
+
+	return buf.String()
+}
+
+func TestPostObjectHandler(t *testing.T) {
+	cfg := testutils.LoadTestConfig()
+	storage := mocks.NewStorageMock()
+
+	type want struct {
+		code int
+		rsp  string
+	}
+	tests := []struct {
+		name            string
+		rqsData         string
+		contentType     string
+		contentEncoding string
+		acceptEncoding  string
+		want            want
+	}{
+		{
+			name:            "common",
+			rqsData:         compress(`{"url":"https://www.foo.com"}`),
+			contentType:     "application/json",
+			contentEncoding: "gzip",
+			acceptEncoding:  "gzip",
+			want: want{
+				code: 201,
+				rsp:  compress(`{"result":"http://localhost:8080/6ySFbLgd"}`),
+			},
+		},
+		{
+			name:            "no compress",
+			rqsData:         `{"url":"https://www.foo.com"}`,
+			contentType:     "application/json",
+			contentEncoding: "",
+			acceptEncoding:  "",
+			want: want{
+				code: 201,
+				rsp:  `{"result":"http://localhost:8080/6ySFbLgd"}`,
+			},
+		},
+		{
+			name:            "compress_rsp",
+			rqsData:         `{"url":"https://www.foo.com"}`,
+			contentType:     "application/json",
+			contentEncoding: "",
+			acceptEncoding:  "gzip",
+			want: want{
+				code: 201,
+				rsp:  compress(`{"result":"http://localhost:8080/6ySFbLgd"}`),
+			},
+		},
+		{
+			name:            "compressed_rqs",
+			rqsData:         compress(`{"url":"https://www.foo.com"}`),
+			contentType:     "application/json",
+			contentEncoding: "gzip",
+			acceptEncoding:  "",
+			want: want{
+				code: 201,
+				rsp:  `{"result":"http://localhost:8080/6ySFbLgd"}`,
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			urlService := service.NewURLService(storage)
+
+			handler := handler.NewURLHandler(urlService, &cfg)
+			router := chi.NewRouter()
+			router.Post("/api/shorten", Compress(handler.ProcessPostObject))
+
+			rqs := httptest.NewRequest(http.MethodPost, "/api/shorten", bytes.NewBufferString(test.rqsData))
+			rqs.Header.Set("Content-Type", test.contentType)
+			rqs.Header.Set("Content-Encoding", test.contentEncoding)
+			rqs.Header.Set("Accept-Encoding", test.acceptEncoding)
+
+			rsp := httptest.NewRecorder()
+			router.ServeHTTP(rsp, rqs)
+
+			res := rsp.Result()
+			defer res.Body.Close()
+
+			assert.Equal(t, test.want.code, res.StatusCode)
+			resBody, _ := io.ReadAll(res.Body)
+			assert.Equal(t, test.want.rsp, string(resBody))
+		})
+	}
+}
